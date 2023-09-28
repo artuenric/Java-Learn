15f7b6e4d2d39f6c4569ccef7556b971cbd6dbf7
artuenric <artuenrick@gmail.com>
2023-09-28 14:02:20 -0300

    Create Futebol
    Add o projeto futebol. Estudo de Interface.

--- Futebol/src/main/java/com/mycompany/futebol/Futebol.java
+++ Futebol/src/main/java/com/mycompany/futebol/Futebol.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.estudos;
+package com.mycompany.futebol;
 
 /**
  *
  * @author artue
  */
-public class Estudos {
+public class Futebol {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
